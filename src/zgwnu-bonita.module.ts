2ec2e0ed4c2bbfa531297af17ea02cf0c217ae02
Onno Haldar <onno@zgw.nu>
2017-09-06 00:42:13 +0200

    refactor

--- src/zgwnu-bonita.module.ts
+++ src/zgwnu-bonita.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core'
 
-import { ZgwnuBonitaAuthenticationService } from './authentication'
-import { ZgwnuBonitaBpmProcessService } from './bpm/process'
-import { ZgwnuBonitaBpmCaseService } from './bpm/case'
-import { ZgwnuBonitaBpmCaseDocumentService } from './bpm/document' 
-import { ZgwnuBonitaBpmDataService } from './bpm/data'
-import { ZgwnuBonitaBpmActivityService } from './bpm/activity'
-import { ZgwnuBonitaBpmTaskService } from './bpm/task'
-import { ZgwnuBonitaBpmHumanTaskService } from './bpm/human-task'
-import { ZgwnuBonitaBpmUserTaskService } from './bpm/user-task'
 import { ZgwnuBonitaBusinessDataService } from './bdm'
-import { ZgwnuBonitaFileUploadService } from './file-upload'
+import { ZgwnuBonitaBpmUserTaskService } from './bpm/user-task'
+import { ZgwnuBonitaBpmHumanTaskService } from './bpm/human-task'
+import { ZgwnuBonitaBpmTaskService } from './bpm/task'
+import { ZgwnuBonitaBpmActivityService } from './bpm/activity'
+import { ZgwnuBonitaBpmDataService } from './bpm/data'
+import { ZgwnuBonitaBpmCaseDocumentService } from './bpm/document' 
+import { ZgwnuBonitaBpmCaseService } from './bpm/case'
+import { ZgwnuBonitaBpmProcessService } from './bpm/process'
+import { ZgwnuBonitaAuthenticationService } from './authentication'
 import { ZgwnuBonitaConfigService, ZgwnuBonitaRestApiService } from './rest-api' 
+import { ZgwnuBonitaFileUploadService } from './file-upload'
 
 @NgModule({
     providers: [ 
