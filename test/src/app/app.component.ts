3ba0fd147ccae919a5ac82ce8f71f91d46567991
Onno Haldar <onno@zgw.nu>
2017-09-08 09:05:03 +0200

    r

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component, OnInit } from '@angular/core'
-import { ZgwnuBonitaAuthenticationService, ZgwnuBonitaCredentials } from '../ng-bonita/src'
 
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
-  providers: [ ZgwnuBonitaAuthenticationService ],
 })
 export class AppComponent implements OnInit { 
 
   public name = 'Angular'
 
   constructor(
-    private authenticationService: ZgwnuBonitaAuthenticationService,
   )
   {}
 
--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule }           from '@angular/core'
 import { BrowserModule }      from '@angular/platform-browser'
-import { ZgwnuNgBonitaModule } from '../ng-bonita/src'
 
 import { AppComponent }  from './app.component'
 
 @NgModule({
   imports: [ 
     BrowserModule,
-    ZgwnuNgBonitaModule, 
    ],
   declarations: [ AppComponent ],
   providers: [ ],
