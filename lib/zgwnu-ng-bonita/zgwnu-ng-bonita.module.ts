4b6b4d8681ced4d3789e2bfbe12bd1f97e04eb4f
Onno Haldar <onno@zgw.nu>
2017-09-08 16:42:30 +0200

    refactor

--- lib/zgwnu-ng-bonita/zgwnu-ng-bonita.module.ts
+++ lib/zgwnu-ng-bonita/zgwnu-ng-bonita.module.ts
@@ -1,12 +1,10 @@
 import { NgModule }     from '@angular/core'
-import { CommonModule } from '@angular/common'
 import { HttpModule }   from '@angular/http'
 
 import { ZgwnuBonitaAuthenticationService } from './authentication/zgwnu-bonita-authentication.service'
 
 @NgModule({
   imports: [
-    CommonModule, 
     HttpModule,  
    ],
   providers: [
