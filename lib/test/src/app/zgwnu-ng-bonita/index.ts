ee536f673230d732fa3ac925c0a06a28ee4119a9
Onno Haldar <onno@zgw.nu>
2017-09-09 13:54:23 +0200

    refactor

--- lib/test/src/app/zgwnu-ng-bonita/index.ts
+++ lib/test/src/app/zgwnu-ng-bonita/index.ts
@@ -1,3 +1,7 @@
+// Module With Providers
 export { ZgwnuNgBonitaModule } from './zgwnu-ng-bonita.module'
 
+// Rest Api (Service) Classes that shlould be available Outside Module
 export { ZgwnuBonitaConfigService } from './rest-api/zgwnu-bonita-config.service'
+export { ZgwnuBonitaDataMappingInterface } from './rest-api/zgwnu-bonita-data-mapping.interface'
+
