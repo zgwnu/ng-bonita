51db583dc08fc1a0acfd62d8fc386e8d25f3ef1e
Onno Haldar <onno@zgw.nu>
2017-09-09 19:38:41 +0200

    refactor

--- lib/zgwnu-ng-bonita/index.ts
+++ lib/zgwnu-ng-bonita/index.ts
@@ -1,53 +1,10 @@
-// Bonita Angular Module(s)
+// Module With Bonita Service Providers
 export { ZgwnuNgBonitaModule } from './zgwnu-ng-bonita.module'
 
-// Bonita Authentication
-export { ZgwnuBonitaCredentials } from './authentication/zgwnu-bonita-credentials'
-export { ZgwnuBonitaAuthenticationService } from './authentication/zgwnu-bonita-authentication.service'
-
-// Bonita BDM (Business Data Management)
-export { ZgwnuBonitaBusinessDataService } from './bdm/zgwnu-bonita-business-data.service'
-export { ZgwnuBonitaBusinessDataContext } from './bdm/zgwnu-bonita-business-data-context' 
-export { ZgwnuBonitaBusinessDataObject } from './bdm/zgwnu-bonita-business-data-object'
-export { ZgwnuBonitaBusinessDataQueryParms } from './bdm/zgwnu-bonita-business-data-query-parms'
-
-// Bonita BPM (Business Process Management)
-
-// Bonita BPM Activity
-
-
-// Bonita BPM Case
-
-
-// Bonita BPM Data
-
-
-// Bonita BPM Document
-
-
-// Bonita BPM Human Task
-
-
-// Bonita BPM Process
-
-
-// Bonita BPM Task
-
-
-// Bonita BPM User Task
-
-
-
-// Bonita File Upload
-
-
-// Bonita Rest Api (Common)
+// Make Rest Api (Service) Classes available outside Module
 export { ZgwnuBonitaConfigService } from './rest-api/zgwnu-bonita-config.service'
 export { ZgwnuBonitaDataMappingInterface } from './rest-api/zgwnu-bonita-data-mapping.interface'
-export { ZgwnuBonitaDataMapping } from './rest-api/zgwnu-bonita-data-mapping'
-export { ZgwnuBonitaErrorResponse } from './rest-api/zgwnu-bonita-error-response'
-export { ZgwnuBonitaResponse } from './rest-api/zgwnu-bonita-response'
-export { ZgwnuBonitaRestApiService } from './rest-api/zgwnu-bonita-rest-api.service'
-export { ZgwnuBonitaSessionMapping } from './rest-api/zgwnu-bonita-session-mapping'
-export { ZgwnuBonitaUrls } from './rest-api/zgwnu-bonita-urls'
-export { ZgwnuBonitaUtils } from './rest-api/zgwnu-bonita-utils'
+
+// Authentication (Service) Classes available outside Module
+export { ZgwnuBonitaAuthenticationService } from './authentication/zgwnu-bonita-authentication.service'
+export { ZgwnuBonitaCredentials } from './authentication/zgwnu-bonita-credentials'
--- lib/zgwnu-ng-bonita/zgwnu-ng-bonita.module.ts
+++ lib/zgwnu-ng-bonita/zgwnu-ng-bonita.module.ts
@@ -1,13 +1,17 @@
 import { NgModule }     from '@angular/core'
 import { HttpModule }   from '@angular/http'
 
+import { ZgwnuBonitaConfigService } from './rest-api/zgwnu-bonita-config.service'
 import { ZgwnuBonitaAuthenticationService } from './authentication/zgwnu-bonita-authentication.service'
+import { ZgwnuBonitaBackendService } from './rest-api/zgwnu-bonita-backend.service'
 
 @NgModule({
   imports: [
     HttpModule,  
    ],
   providers: [
+    ZgwnuBonitaConfigService, 
+    ZgwnuBonitaBackendService, 
     ZgwnuBonitaAuthenticationService, 
   ]
 })
