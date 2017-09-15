3c171e7b073e2e99f337f077038be64001573ec2
Onno Haldar <onno@zgw.nu>
2017-09-15 11:32:11 +0200

    refactor

--- bpm/process/zgwnu-bonita-bpm-process.service.ts
+++ bpm/process/zgwnu-bonita-bpm-process.service.ts
@@ -7,7 +7,7 @@
 
 // ANGULAR Imports
 import { Injectable } from '@angular/core'
-import { HttpHeaders, HttpClient } from '@angular/common/http'
+import { HttpHeaders, HttpClient, HttpResponse } from '@angular/common/http'
 
 // RXJS Imports
 import { Observable } from 'rxjs/Observable'
@@ -17,9 +17,11 @@ import 'rxjs/add/operator/catch'
 // ZGWNU Ng Bonita Module Imports
 import { ZgwnuBonitaConfigService } from '../../rest-api/zgwnu-bonita-config.service'
 import { ZgwnuBonitaResponseMapService } from '../../rest-api/zgwnu-bonita-response-map.service'
+import { ZgwnuBonitaResponse } from '../../rest-api/zgwnu-bonita-response'
 import { ZgwnuBonitaSearchParms } from '../zgwnu-bonita-search-parms'
 import { ZgwnuBonitaProcessDefinitionDataInterface } from './zgwnu-bonita-process-definition-data.interface'
 import { ZgwnuBonitaProcessDefinition } from './zgwnu-bonita-process-definition'
+import { ZgwnuBonitaCreateCaseSuccessResponse } from './zgwnu-bonita-create-case-success-response'
 
 @Injectable()
 export class ZgwnuBonitaBpmProcessService {
@@ -60,6 +62,40 @@ export class ZgwnuBonitaBpmProcessService {
     private mapProcessDefinition(body: ZgwnuBonitaProcessDefinitionDataInterface): ZgwnuBonitaProcessDefinition {
         let processDefinition: ZgwnuBonitaProcessDefinition = new ZgwnuBonitaProcessDefinition(body)
         return processDefinition
+    }
+
+
+    // Start a process using an instantiation contract
+    //
+    // based on: http://documentation.bonitasoft.com/?page=bpm-api#toc23
+    //
+    // Post URL template: ../API/bpm/process/:processId/instantiation
+    //
+    createCase(processId: string, contractValues: any): Observable<ZgwnuBonitaCreateCaseSuccessResponse> {
+        console.log('ZgwnuBonitaBpmProcessService.createCase')
+        console.log(this.configService.session)
+        let postUrl: string = this.resourceUrl + '/' + processId + '/instantiation'
+        let sendHeaders: HttpHeaders = new HttpHeaders().set(
+                this.configService.bonitaSessionTokenKey, this.configService.session.token)
+        return this.httpClient.post(
+            postUrl,
+            contractValues,
+            {
+                headers: sendHeaders,
+                observe: 'response',
+                responseType: 'json'
+            }
+        )
+        .map(this.mapCreateCaseSuccessResponse)
+        .catch(this.responseMapService.catchBonitaError)
+    }
+
+    private mapCreateCaseSuccessResponse(response: HttpResponse<Object>): ZgwnuBonitaCreateCaseSuccessResponse {
+        let successResponse: ZgwnuBonitaCreateCaseSuccessResponse = new ZgwnuBonitaCreateCaseSuccessResponse()
+        successResponse.status = response.status
+        successResponse.statusText = response.statusText
+        successResponse.caseId = response.body['caseId']
+        return successResponse
     }
 
 }
