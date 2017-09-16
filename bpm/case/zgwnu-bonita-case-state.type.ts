254547377e6826d7ee746a35aaea1018ce2dde95
Onno Haldar <onno@zgw.nu>
2017-09-16 16:57:56 +0200

    refactor

--- bpm/case/zgwnu-bonita-case-state.type.ts
+++ bpm/case/zgwnu-bonita-case-state.type.ts
@@ -1,2 +1,3 @@
-// state: an enum that represent the state of the case
+// state: an enum that represent the state of the case (documented as CAPS but system gives NON-CAPS)
 export declare type ZgwnuBonitaCaseStateType = 'INITIALIZING' | 'STARTED' | 'SUSPENDED' | 'CANCELLED' | 'ABORTED' | 'COMPLETING' | 'COMPLETED' | 'ERROR' | 'ABORTING'
+    | 'initializing' | 'started' | 'suspended' | 'cancelled' | 'aborted' | 'completing' | 'completed' | 'error' | 'aborting'
