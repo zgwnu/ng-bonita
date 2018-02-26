33799cf0b1201aa87cdb72d5a54a94f92f50b4c7
Onno Haldar <onno@zgw.nu>
2018-02-26 15:51:27 +0100

    Rfecator

--- authentication/zgwnu-bonita-authentication.service.ts
+++ authentication/zgwnu-bonita-authentication.service.ts
@@ -11,11 +11,14 @@ import { HttpHeaders, HttpClient } from '@angular/common/http'
 
 // RXJS Imports
 import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/observable/throw'
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 
 // ZGWNU Ng Bonita Module Imports
 import { ZgwnuBonitaConfigService } from '../rest-api/zgwnu-bonita-config.service'
+import { ZgwnuBonitaSessionService } from '../rest-api/zgwnu-bonita-session.service'
+import { ZgwnuBonitaSession } from '../rest-api/zgwnu-bonita-session'
 import { ZgwnuBonitaResponseMapService } from '../rest-api/zgwnu-bonita-response-map.service'
 import { ZgwnuBonitaResponse } from '../rest-api/zgwnu-bonita-response'
 import { ZgwnuBonitaCredentials } from './zgwnu-bonita-credentials'
@@ -27,10 +30,32 @@ export class ZgwnuBonitaAuthenticationService {
     constructor(
         private httpClient: HttpClient, 
         private configService: ZgwnuBonitaConfigService,  
+        private sessionService: ZgwnuBonitaSessionService,  
         private responseMapService: ZgwnuBonitaResponseMapService,  
     )
     {
     }
+
+    loginNew(creds: ZgwnuBonitaCredentials): Observable<ZgwnuBonitaSession> {
+        return new Observable<ZgwnuBonitaSession>(observer => {
+            this.login(creds)
+                .subscribe(
+                    response => {
+                        this.sessionService.getSession()
+                            .subscribe(
+                                session => {
+                                    observer.next(session)
+                                    observer.complete()
+                                },
+                                error => observer.syncErrorValue(error)
+                            )
+                    },
+                    error => observer.syncErrorValue(error)
+                )
+            }
+        );
+    }
+
 
     login(creds: ZgwnuBonitaCredentials): Observable<ZgwnuBonitaResponse> {
         let loginUrl: string = this.configService.bonitaUrls.baseUrl + this.LOGIN_SERVICE_PATH
